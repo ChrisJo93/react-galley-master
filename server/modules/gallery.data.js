f2980519b897b09228f39dba600fe4abf59c5f9e
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-31 18:40:50 -0500

    added image objects

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,27 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: 'images/goat_small.jpg',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'images/fatboi.jpg',
+    description: 'Photo of a Black Rain Frog in South Africa.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'images/longnek.png',
+    description: 'Photo of a Giraffe of a South African Zoo.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'images/lordbearon.jpg',
+    description: 'Photo of a Brown Grizzly Bear of the Northern United States.',
+  },
 ];
 
 module.exports = galleryItems;
