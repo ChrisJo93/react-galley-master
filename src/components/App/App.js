35f597a60b381baff4eed8507088638acbcd1644
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-31 23:55:25 -0500

    pictures appear in block display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -50,7 +50,7 @@ class App extends Component {
             );
           })}
         </div>
-        <img src="images/goat_small.jpg" />
+        {/* <img src="images/goat_small.jpg" /> */}
       </div>
     );
   }
