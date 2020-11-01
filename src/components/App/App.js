87f51a4ca6ad06b6f9401e52e8f9aadb37c35e2e
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-31 21:27:38 -0500

    created get for gallery items

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,34 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
-import galleryItems from '../../../server/modules/gallery.data';
+// import galleryItems from '../../../server/modules/gallery.data';
 import './App.css';
 // import GalleryList from './GalleryList';
 
-console.log(galleryItems);
-
 class App extends Component {
+  state = {
+    galleryList: [],
+    errorMsg: null,
+  };
+
+  getGallery() {
+    Axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          galleryList: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          errorMsg: 'lol',
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
