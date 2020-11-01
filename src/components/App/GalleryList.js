335d51eca3c6548577e6fca43029b75b82c268fa
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-01 01:24:24 -0500

    added onclick feature to like button

--- src/components/App/GalleryList.js
+++ src/components/App/GalleryList.js
@@ -1,13 +1,22 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 
 class GalleryList extends Component {
+  addLike() {
+    Axios({
+      method: 'PUT',
+      url: '/like/:id',
+    });
+  }
+
   render() {
     const gallery = this.props.gallery.map((item) => {
       return (
         <div className="gallery" key={item.id}>
           <img src={item.path} />
-          <div>{item.description}</div>
-          <button>{item.likes}</button>
+          {/* <div>{item.description}</div> */}
+          <p> </p>
+          <button onClick={this.addLike}>{item.likes}</button>
         </div>
       );
     });
