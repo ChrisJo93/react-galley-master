b2c98ee6dd2f89a71a47aa8fe1952ed7a8e209c4
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-01 15:22:24 -0600

    pictures toggle to descriptions on click

--- src/components/App/GalleryItem.js
+++ src/components/App/GalleryItem.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+  state = {
+    picToggled: true,
+  };
+
+  toggleHandler = () => {
+    this.setState({
+      picToggled: this.state.picToggled === false,
+    });
+  };
+
+  togglePic = () => {
+    if (this.state.picToggled === true) {
+      return (
+        <img
+          src={this.props.item.path}
+          alt={this.props.item.description}
+          className="gallery-items"
+          onClick={this.toggleHandler}
+        />
+      );
+    } else {
+      return (
+        <div onClick={this.toggleHandler}>
+          <p>{this.props.item.description}</p>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div className="gallery-items-container">
-        <img src={this.props.item.path} className="gallery-items" />
+        <div>{this.togglePic()}</div>
         <button onClick={() => this.props.putLikes(this.props.item)}>
           Likes {this.props.item.likes}
         </button>
