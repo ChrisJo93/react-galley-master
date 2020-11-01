cc529c3f74293efc12b075f9050a78a34889f388
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-31 23:18:29 -0500

    Gallery items made it to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,6 @@
 import Axios from 'axios';
 import React, { Component } from 'react';
-// import galleryItems from '../../../server/modules/gallery.data';
 import './App.css';
-// import GalleryList from './GalleryList';
 
 class App extends Component {
   state = {
@@ -10,13 +8,17 @@ class App extends Component {
     errorMsg: null,
   };
 
+  componentDidMount() {
+    this.getGallery();
+  }
+
   getGallery() {
     Axios({
       method: 'GET',
       url: '/gallery',
     })
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
         this.setState({
           galleryList: response.data,
         });
@@ -37,8 +39,7 @@ class App extends Component {
         </header>
         <br />
         <p>Gallery goes here</p>
-        {/* <GalleryList />
-        <GalleryItem /> */}
+
         <img src="images/goat_small.jpg" />
       </div>
     );
